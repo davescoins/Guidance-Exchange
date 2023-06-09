72e1901da6c616457394fa241cf2d6604377fb09
BTcapstone <134342991+BTcapstone@users.noreply.github.com>
2023-06-09 19:12:24 -0400

    added hero image and change .html back to .php

--- navbar-template.php
+++ navbar-template.php
@@ -50,8 +50,6 @@
   <!-- Content Section -->
   <section>Content Here!</section>
 
-
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/c5863419fe.js" crossorigin="anonymous"></script>
