946399b30a05e923bdc432a498f9f9e2d519d813
bernieYanos <21007971+bernieYanos@users.noreply.github.com>
2023-06-09 14:54:01 -0400

    created requests section

--- moderator-template.php
+++ moderator-template.php
@@ -87,7 +87,241 @@
 
   <!-- Content Section -->
   <section class="content-section pt-4">
+    <div class="container-fluid w-50 pb-4 flex-column">
+      <div class="d-flex align-items-start w-100">
+        <h4>Mentor Requests</h4>
+      </div>
+      <div class="accordion w-100 row" id="mentor-requests">
 
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#mentor_collapse1" aria-expanded="false" aria-controls="mentor_collapse1">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/benjamin-park.png" alt="Benjamin Park Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Benjamin Park</h4>
+              </div>
+            </button>
+          </div>
+          <div id="mentor_collapse1" class="accordion-collapse collapse" data-bs-parent="#mentor-requests">
+            <div class="accordion-body">
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3">
+                <a href="#"><img class="moderator__icons" src="img/GElogo_blue.png" alt="Guidance Exchange Profile"></a>
+                <a href="#"><i class="fa-brands fa-linkedin moderator__icons"></i></a>
+                <a href="#"><i class="fa-solid fa-file-pdf moderator__icons"></i></a>
+              </div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#mentor_collapse2" aria-expanded="false" aria-controls="mentor_collapse2">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/harper-brown.png" alt="Harper Brown Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Harper Brown</h4>
+              </div>
+            </button>
+          </div>
+          <div id="mentor_collapse2" class="accordion-collapse collapse" data-bs-parent="#mentor-requests">
+            <div class="accordion-body">
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3">
+                <a href="#"><img class="moderator__icons" src="img/GElogo_blue.png" alt="Guidance Exchange Profile"></a>
+                <a href="#"><i class="fa-brands fa-linkedin moderator__icons"></i></a>
+                <a href="#"><i class="fa-solid fa-file-pdf moderator__icons"></i></a>
+              </div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#mentor_collapse3" aria-expanded="false" aria-controls="mentor_collapse3">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/lucas-khan.png" alt="Lucas Khan Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Lucas Khan</h4>
+              </div>
+            </button>
+          </div>
+          <div id="mentor_collapse3" class="accordion-collapse collapse" data-bs-parent="#mentor-requests">
+            <div class="accordion-body">
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3">
+                <a href="#"><img class="moderator__icons" src="img/GElogo_blue.png" alt="Guidance Exchange Profile"></a>
+                <a href="#"><i class="fa-brands fa-linkedin moderator__icons"></i></a>
+                <a href="#"><i class="fa-solid fa-file-pdf moderator__icons"></i></a>
+              </div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#mentor_collapse4" aria-expanded="false" aria-controls="mentor_collapse4">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/mia-wilson.png" alt="Mia Wilson Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Mia Wilson</h4>
+              </div>
+            </button>
+          </div>
+          <div id="mentor_collapse4" class="accordion-collapse collapse" data-bs-parent="#mentor-requests">
+            <div class="accordion-body">
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3">
+                <a href="#"><img class="moderator__icons" src="img/GElogo_blue.png" alt="Guidance Exchange Profile"></a>
+                <a href="#"><i class="fa-brands fa-linkedin moderator__icons"></i></a>
+                <a href="#"><i class="fa-solid fa-file-pdf moderator__icons"></i></a>
+              </div>
+              I am a seasoned stock broker with a wealth of experience in managing stock portfolios, and am dedicated to helping aspiring stock traders navigate the dynamic world of investments. With my deep understanding of market trends, risk management strategies, and financial analysis, I aim to offer invaluable mentoring and advice to those looking to enter the exciting realm of stock trading. Drawing from my years of experience in executing trades, analyzing market data, and building successful portfolios, I can provide personalized guidance tailored to individual goals and risk tolerance. My passion for empowering others, coupled with my extensive expertise, makes me an ideal mentor for aspiring stock traders seeking to achieve financial success in the stock market.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#mentor_collapse5" aria-expanded="false" aria-controls="mentor_collapse5">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/sophia-lee.png" alt="Sophia Lee Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Sophia Lee</h4>
+              </div>
+            </button>
+          </div>
+          <div id="mentor_collapse5" class="accordion-collapse collapse" data-bs-parent="#mentor-requests">
+            <div class="accordion-body">
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3">
+                <a href="#"><img class="moderator__icons" src="img/GElogo_blue.png" alt="Guidance Exchange Profile"></a>
+                <a href="#"><i class="fa-brands fa-linkedin moderator__icons"></i></a>
+                <a href="#"><i class="fa-solid fa-file-pdf moderator__icons"></i></a>
+              </div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="d-flex align-items-start w-100 pt-3">
+        <h4>Community Requests</h4>
+      </div>
+      <div class="accordion w-100 row" id="community-requests">
+
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#community_collapse1" aria-expanded="false" aria-controls="community_collapse1">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/java_icon.png" alt="Java Programming Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Java Programming</h4>
+              </div>
+            </button>
+          </div>
+          <div id="community_collapse1" class="accordion-collapse collapse" data-bs-parent="#community-requests">
+            <div class="accordion-body">
+              <h5 class="pt-2">Requested by:</h5>
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3 pt-2 moderator__community-request-profile">
+                <a href="#"><img class="pe-4" src="upload/sophia-lee.png" alt="Sophia Lee Profile Picture"></a>
+                <h6>Sophia Lee</h6>
+              </div>
+              <h5 class="pt-2">Description of Community:</h5>
+              I'm thrilled to propose the creation of a fantastic forum community for Java programming enthusiasts. I'm looking for a place where passionate programmers can come together to discuss, learn, and share knowledge about Java. Imagine a space brimming with like-minded individuals who are just as eager as you are to delve into the intricacies of Java programming. I want a Java community where you can initiate and participate in stimulating conversations, exchange valuable code snippets, seek advice on troubleshooting, and connect with seasoned professionals ready to mentor. Whether you're a beginner seeking guidance or a seasoned expert eager to contribute, the Java community would be a supportive environment where growth and collaboration thrive. Thank you for considering creating this exciting space for Java enthusiasts, and let's unlock endless possibilities for learning and networking within the Java community.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#community_collapse2" aria-expanded="false" aria-controls="community_collapse2">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/stocks_circle.png" alt="Stock Trading Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Stock Trading</h4>
+              </div>
+            </button>
+          </div>
+          <div id="community_collapse2" class="accordion-collapse collapse" data-bs-parent="#community-requests">
+            <div class="accordion-body">
+              <h5 class="pt-2">Requested by:</h5>
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3 pt-2 moderator__community-request-profile">
+                <a href="#"><img class="pe-4" src="upload/mia-wilson.png" alt="Mia Wilson Profile Picture"></a>
+                <h6>Mia Wilson</h6>
+              </div>
+              <h5 class="pt-2">Description of Community:</h5>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <div class="accordion-header">
+            <button class="accordion-button collapsed py-2" type="button" data-bs-toggle="collapse" data-bs-target="#community_collapse3" aria-expanded="false" aria-controls="community_collapse3">
+              <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center justify-content-center moderator__mentor-request-photo">
+                  <img class="pe-4" src="/upload/coins_circle.png" alt="Coin Collecting Profile Photo">
+                </div>
+                <h4 class="moderator__request-header ps-4 mb-0">Coin Collecting</h4>
+              </div>
+            </button>
+          </div>
+          <div id="community_collapse3" class="accordion-collapse collapse" data-bs-parent="#community-requests">
+            <div class="accordion-body">
+              <h5 class="pt-2">Requested by:</h5>
+              <div class="container-fluid d-flex justify-content-start px-0 pb-3 pt-2 moderator__community-request-profile">
+                <a href="#"><img class="pe-4" src="upload/lucas-khan.png" alt="Lucas Khan Profile Picture"></a>
+                <h6>Lucas Khan</h6>
+              </div>
+              <h5 class="pt-2">Description of Community:</h5>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti.
+              <div class="container-fluid pt-3">
+                <button type="button" class="btn btn-success mx-4">Approve</button>
+                <button type="button" class="btn btn-danger mx-4">Deny</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   </section>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
