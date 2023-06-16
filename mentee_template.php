d3096bac434d5903edec8a4c7aa9f5b86f9fcd1c
Bernie Yanos <21007971+bernieYanos@users.noreply.github.com>
2023-06-16 13:59:41 -0400

    moved css to separate file

--- mentee_template.php
+++ mentee_template.php
@@ -8,6 +8,7 @@
   <title>Profile Template</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+  <link rel="stylesheet" href="css/profile.css" />
   <link rel="stylesheet" href="css/main.css" />
 </head>
 
