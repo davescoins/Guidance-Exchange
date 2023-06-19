123cd29330b04909536fce05cc99bc0cfa0b2277
dave miller <davescoins@gwu.edu>
2023-06-19 15:32:21 -0400

    community page

---
+++ community.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Navbar Template</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+  <link rel="stylesheet" href="css/sysadmin2.css" />
+</head>
+<body>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="home.php"><img src="img/logo_gradient.png" alt="Guidance Exchange Logo" height="70" /></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link highlight-link nav-text px-4" href="#">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link highlight-link nav-text px-4" href="#">Communities</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link highlight-link nav-text px-4" href="#">Mentoring</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav d-flex flex-row me-1">
+          <li class="nav-item me-3 me-lg-0 px-2">
+            <a class="nav-link" href="#"><i class="fa-solid fa-magnifying-glass fa-xl"></i></i></i></a>
+          </li>
+          <li class="nav-item me-3 me-lg-0 px-2">
+            <a class="nav-link" href="#"><i class="fa-solid fa-inbox fa-xl"></i></i></a>
+          </li>
+          <li class="nav-item me-3 me-lg-0 px-2">
+            <a class="nav-link" href="#"><i class="fa-solid fa-user-group fa-xl"></i></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+  <!-- Content Section -->
+
+
+
+  <footer>
+  <nav class="navbar">
+    <div class="mx-auto">
+      <div class="my-3">
+        <a href="https://www.facebook.com/" class="mx-3"><i class="fa fa-facebook"></i></a>
+        <a href="https://www.instagram.com/" class="mx-3"><i class="fa fa-instagram"></i></a>
+        <a href="https://www.twitter.com/" class="mx-3"><i class="fa fa-twitter"></i></a>
+        <a href="https://www.linkedin.com" class="mx-3"><i class="fa fa-linkedin"></i></a>
+      </div>
+      <div class="footer-text text-center pb-3">© 2023 Guidance Exchange</div>
+    </div>
+  </nav>
+</footer>
+</html>
+
+
