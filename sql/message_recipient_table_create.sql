aeb34b6954a2d781f31aad5ec6e63b374ef14086
Bernie Yanos <21007971+bernieYanos@users.noreply.github.com>
2023-07-12 09:32:47 -0400

    modified table structure to allow for reply messages

---
+++ sql/message_recipient_table_create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Message_Recipient_t (
+  MessageRecipientID INT(9) NOT NULL Auto_Increment,
+  MessageID INT(9) NOT NULL,
+  RecipientID INT(9) NOT NULL,
+  IsRead BOOL NOT NULL,
+  CONSTRAINT Message_Recipient_t_PK PRIMARY KEY (MessageRecipientID),
+  CONSTRAINT Message_Recipient_t_FK1 FOREIGN KEY (MessageID) REFERENCES Messages_t(MessageID),
+  CONSTRAINT Message_Recipient_t_FK2 FOREIGN KEY (RecipientID) REFERENCES Auth_t(UserID)
+) Auto_Increment = 1;
--- sql/messages_data_insert.sql
+++ sql/messages_data_insert.sql
@@ -1,149 +1,220 @@
 INSERT INTO
   Messages_t (
     `SenderID`,
-    `RecipientID`,
     `MessageSubject`,
     `MessageBody`,
-    `SendDate`
+    `SendDate`,
+    `ParentMessageID`
   )
 VALUES
   (
     1,
-    2,
     'Career Advice',
     'I have been working in the software industry for a few years now, and I\'m at a point where I want to take my career to the next level. I would greatly appreciate any advice or insights you can provide regarding career growth, skill development, and potential opportunities.',
-    '2023-07-03 06:10:00'
+    '2023-07-03 06:10:00',
+    NULL
   ),
   (
     3,
-    5,
     'Mentoring Opportunity',
     'I have been following your successful career journey and I\'m impressed by your achievements. I believe that having you as a mentor would greatly benefit my professional growth. I am eager to learn from your experiences, receive guidance on navigating challenges, and gain insights into the industry trends.',
-    '2023-07-10 12:45:00'
+    '2023-07-10 12:45:00',
+    NULL
   ),
   (
     4,
-    1,
     'Seeking Career Advice',
     'I am currently at a crossroads in my career, and I\'m seeking advice from experienced professionals like yourself. I would like to discuss my long-term goals, potential career paths, and strategies for advancing in my field. Your guidance and wisdom would be invaluable to me.',
-    '2023-07-12 05:40:00'
+    '2023-07-12 05:40:00',
+    NULL
   ),
   (
     2,
-    6,
     'Mentoring Request',
     'I have been following your career trajectory with great interest, and I am inspired by your achievements. I would like to request your mentorship and guidance as I navigate my own professional journey. Your insights, feedback, and support would be immensely valuable in helping me achieve my career goals.',
-    '2023-07-17 00:40:00'
+    '2023-07-17 00:40:00',
+    NULL
   ),
   (
     5,
-    3,
     'Career Advice Needed',
     'I am considering transitioning into a new career field and I\'m looking for advice on how to successfully make the switch. I would appreciate any insights you can share regarding the skills I should acquire, the challenges I may face, and the best strategies to make a smooth transition.',
-    '2023-07-20 15:30:00'
+    '2023-07-20 15:30:00',
+    NULL
   ),
   (
     6,
-    4,
     'Seeking Mentor',
     'I have recognized the importance of mentorship in career development, and I am actively seeking a mentor to guide me. I believe that your expertise and experience align perfectly with my aspirations. I would be grateful for the opportunity to learn from you and receive guidance on my professional journey.',
-    '2023-07-25 16:50:00'
+    '2023-07-25 16:50:00',
+    NULL
   ),
   (
     1,
-    3,
     'Career Transition Advice',
     'I have been contemplating a career change for some time now, but I\'m unsure about the best approach. I would greatly appreciate your advice on how to successfully navigate this transition. I would like to discuss the steps I should take, the potential challenges I may encounter, and any opportunities you may be aware of.',
-    '2023-08-02 00:35:00'
+    '2023-08-02 00:35:00',
+    NULL
   ),
   (
     2,
-    4,
     'Mentoring Inquiry',
     'I\'m highly interested in exploring mentoring opportunities with professionals like yourself. I am eager to gain insights into your journey, learn from your experiences, and receive guidance on how to overcome obstacles and achieve my career objectives. I would be grateful for the chance to discuss further and explore the potential of a mentor-mentee relationship.',
-    '2023-08-05 03:45:00'
+    '2023-08-05 03:45:00',
+    NULL
   ),
   (
     3,
-    6,
     'Career Guidance Needed',
     'I have reached a point in my career where I feel stuck and uncertain about the next steps. I would greatly appreciate your suggestions and guidance on how to progress and overcome this hurdle. Your expertise and perspectives would be invaluable in helping me regain direction and momentum.',
-    '2023-08-10 14:35:00'
+    '2023-08-10 14:35:00',
+    NULL
   ),
   (
     4,
-    5,
     'Mentoring Assistance',
     'I have been facing several challenges in my professional life, and I believe that having a mentor would provide valuable support. I am seeking a mentor who can guide me through these challenges, help me develop essential skills, and offer advice on how to navigate the complexities of the industry. Your mentorship would be immensely beneficial to me.',
-    '2023-08-13 02:25:00'
+    '2023-08-13 02:25:00',
+    NULL
   ),
   (
     5,
-    4,
     'Career Transition Advice',
     'I have been working in my current field for several years, but I feel like it\'s time for a change. I\'m interested in exploring new career opportunities and would appreciate your advice on how to successfully transition into a different industry. I\'m particularly curious about the skills and qualifications I should focus on acquiring.',
-    '2023-07-01 11:55:00'
+    '2023-07-01 11:55:00',
+    NULL
   ),
   (
     3,
-    2,
     'Mentorship Request',
     'Your accomplishments and expertise in the industry have inspired me, and I believe that having you as a mentor would greatly benefit my professional growth. I would love to learn from your experiences, seek guidance on building a successful career, and discuss strategies for overcoming challenges. Your mentorship would mean a lot to me.',
-    '2023-07-05 15:25:00'
+    '2023-07-05 15:25:00',
+    NULL
   ),
   (
     1,
-    6,
     'Seeking Career Guidance',
     'As a recent graduate, I\'m excited to embark on my professional journey, but I\'m also feeling a bit lost.I would appreciate any career guidance or advice you can offer.I\'m curious to learn about the career paths you took, the challenges you faced, and any lessons you can share to help me kickstart my own career on the right foot.',
-    '2023-07-09 01:45:00'
+    '2023-07-09 01:45:00',
+    NULL
   ),
   (
     2,
-    5,
     'Mentoring Opportunity',
     'I have been following your impressive career trajectory and have always admired your achievements. I\'m seeking a mentor who can provide guidance, support, and insights to help me grow professionally. Your expertise and industry knowledge would be invaluable in helping me navigate the challenges and make informed career decisions.',
-    '2023-07-14 09:40:00'
+    '2023-07-14 09:40:00',
+    NULL
   ),
   (
     6,
-    1,
     'Career Advice Needed',
     'I\'m at a crossroads in my career and could use some advice. I\'m torn between pursuing higher education or gaining more work experience. I would appreciate your perspective on the pros and cons of each path and any recommendations you may have based on your own experiences. Your guidance would help me make an informed decision.',
-    '2023-07-18 03:10:00'
+    '2023-07-18 03:10:00',
+    NULL
   ),
   (
     4,
-    3,
     'Mentoring Request',
     'I have been following your contributions in the industry and have been inspired by your achievements. I would like to request your mentorship to help me develop the necessary skills, overcome challenges, and advance in my career. Your guidance and mentorship would be invaluable as I work towards my professional goals.',
-    '2023-07-23 14:00:00'
+    '2023-07-23 14:00:00',
+    NULL
   ),
   (
     5,
-    2,
     'Career Transition Advice',
     'I have recently decided to change my career path, and I\'m in the process of exploring different options. I would appreciate any advice or insights you can provide on successfully transitioning into a new field. I\'m particularly interested in understanding how to leverage my existing skills and experiences in this transition.',
-    '2023-07-28 16:35:00'
+    '2023-07-28 16:35:00',
+    NULL
   ),
   (
     1,
-    4,
     'Mentoring Inquiry',
     'I\'m interested in finding a mentor who can help me grow as a professional. Your extensive experience and expertise align perfectly with my aspirations. I would be honored to receive guidance from you on topics such as career development, networking, and personal growth. I\'m eager to explore the potential of a mentor-mentee relationship.',
-    '2023-08-01 08:20:00'
+    '2023-08-01 08:20:00',
+    NULL
   ),
   (
     3,
-    6,
     'Career Growth Advice',
     'I\'m in a phase of my career where I\'m looking to take on more challenging roles and advance in my field. I would greatly appreciate your advice on how to effectively position myself for growth opportunities, expand my professional network, and develop the skills necessary to excel in higher-level roles. Your insights would be invaluable to me.',
-    '2023-08-06 09:50:00'
+    '2023-08-06 09:50:00',
+    NULL
   ),
   (
     2,
-    1,
     'Mentoring Assistance',
     'I\'m facing some career-related obstacles and would greatly benefit from having a mentor to guide me. I\'m seeking someone who can provide advice, offer a fresh perspective on my challenges, and help me overcome obstacles. Your mentorship would be highly valued as I work towards achieving my career aspirations.',
-    '2023-08-10 14:20:00'
+    '2023-08-10 14:20:00',
+    NULL
+  ),
+  (
+    2,
+    'Re: Career Advice',
+    'I appreciate your eagerness to take your career to the next level. One piece of advice I can offer is to focus on continuous learning and skill development. Stay updated with the latest industry trends and technologies to remain competitive.',
+    '2023-07-04 08:45:00',
+    1
+  ),
+  (
+    5,
+    'Re: Mentoring Opportunity',
+    'Thank you for reaching out and expressing your interest in having me as your mentor. I\'m honored by your request. I believe mentorship can play a crucial role in one\'s career growth, and I\'m excited to embark on this mentoring journey with you.',
+    '2023-07-11 11:55:00',
+    2
+  ),
+  (
+    1,
+    'Re: Seeking Career Advice ',
+    'I\'m glad you reached out for career advice. Exploring different paths and seeking guidance is a wise move. Let\'s schedule a conversation where we can discuss your long - term goals, assess your skills, and explore potential career opportunities together.',
+    ' 2023-07-15 13:20:00',
+    3
+  ),
+  (
+    6,
+    'Re: Mentoring Request',
+    'I appreciate your interest in my mentorship. It\'s always rewarding to help aspiring professionals like yourself. I would be happy to guide you on your professional journey. Let\'s set up a time to discuss your goals and create a plan to achieve them.',
+    '2023-07-20 15:50:00',
+    4
+  ),
+  (
+    3,
+    'Re: Career Advice Needed',
+    'Transitioning into a new career field can be challenging, but with the right strategies, it\'s definitely achievable. I suggest conducting thorough research, networking with professionals in your desired field, and considering relevant certifications or training programs to enhance your skills.',
+    '2023-07-23 09:30:00',
+    5
+  ),
+  (
+    4,
+    'Re: Seeking Mentor',
+    'Mentorship is a valuable resource, and I\'m glad you recognize its importance.I would be honored to be your mentor.Let\'s connect and discuss your specific needs, goals, and how we can make the most of this mentor-mentee relationship.',
+    '2023-07-28 20:10:00',
+    6
+  ),
+  (
+    3,
+    'Re: Career Transition Advice',
+    'Transitioning into a new career path can be both exciting and challenging. I recommend conducting informational interviews with professionals in your desired field, attending industry events, and considering internships or part-time positions to gain hands-on experience and make a smooth transition.',
+    '2023-08-03 07:50:00',
+    7
+  ),
+  (
+    4,
+    'Re: Mentoring Inquiry',
+    'I appreciate your interest in mentoring. I believe mentorship can provide valuable guidance and support. Let\'s schedule a meeting to discuss your career goals,
+    expectations, and how we can establish an effective mentorship dynamic.',
+    ' 2023-08-06 06:35:00',
+    8
+  ),
+  (
+    6,
+    'Re: Career Guidance Needed',
+    'Feeling stuck in your career is a common experience. To regain direction and momentum, I suggest taking a step back to evaluate your goals and values.We can explore various strategies, such as identifying transferable skills, pursuing additional training, or seeking new challenges within your current role.',
+    '2023-08-12 18:15:00',
+    9
+  ),
+  (
+    5,
+    'Re: Mentoring Assistance',
+    'Overcoming professional challenges often requires guidance and support. I\'m glad you\'re seeking a mentor. Let\'s discuss your specific challenges and create an action plan to address them. Together, we can work towards your professional success.',
+    '2023-08-15 17:45:00',
+    10
   );
