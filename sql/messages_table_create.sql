0fa637ffd1cf4f62a5030d072c8eb7c5b2bc9e1d
Bernie Yanos <21007971+bernieYanos@users.noreply.github.com>
2023-07-13 14:50:26 -0400

    removed unused columns and updated test data

--- sql/messages_table_create.sql
+++ sql/messages_table_create.sql
@@ -1,10 +1,8 @@
 CREATE TABLE Messages_t (
   MessageID INT(9) NOT NULL Auto_Increment,
   SenderID INT(9) NOT NULL,
-  MessageSubject VARCHAR(255) NOT NULL,
   MessageBody LONGTEXT NOT NULL,
   SendDate DATETIME NOT NULL,
-  ParentMessageID INT(9),
   CONSTRAINT Messages_t_PK PRIMARY KEY (MessageID),
   CONSTRAINT Messages_t_FK1 FOREIGN KEY (SenderID) REFERENCES Auth_t(UserID)
 ) Auto_Increment = 1;
