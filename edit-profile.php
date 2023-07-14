8ecf13cf7abe165bb5f035f783e90bf42f241927
bernieYanos <21007971+bernieYanos@users.noreply.github.com>
2023-07-14 15:48:08 -0400

    fixed issue with selecting DC as the state

--- edit-profile.php
+++ edit-profile.php
@@ -156,7 +156,7 @@
     'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
     'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
     'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming',
-    'Washington, D.C.'
+    'District of Columbia'
   );
 
   // Create an array of months
