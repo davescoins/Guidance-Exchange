5bdcd113ee5acc253179017758d2a820db8bbdf4
bernieYanos <21007971+bernieYanos@users.noreply.github.com>
2023-07-24 16:02:16 -0400

    modified insert data

--- sql/communityRequest_data_insert.sql
+++ sql/communityRequest_data_insert.sql
@@ -10,20 +10,20 @@ VALUES
     1,
     6,
     ' Java Programming ',
-    ' I \ ' m thrilled to propose the creation of a fantastic forum community for Java programming enthusiasts.I \ 'm looking for a place where passionate programmers can come together to discuss, learn, and share knowledge about Java. Imagine a space brimming with like-minded individuals who are just as eager as you are to delve into the intricacies of Java programming. I want a Java community where you can initiate and participate in stimulating conversations, exchange valuable code snippets, seek advice on troubleshooting, and connect with seasoned professionals ready to mentor. Whether you\'re a beginner seeking guidance or a seasoned expert eager to contribute, the Java community would be a supportive environment where growth and collaboration thrive. Thank you for considering creating this exciting space for Java enthusiasts, and let\'s unlock endless possibilities for learning and networking within the Java community.',
+    'Welcome to the ultimate online community forum for Java Programming enthusiasts! Join a vibrant community of developers and learners from all levels, where you can share your knowledge, ask questions, and engage in discussions about Java-related topics. Whether you\'re a seasoned programmer or just starting your coding journey, Guidance Exchange is the perfect place to connect, collaborate, and grow together in the world of Java Programming.',
     'java_icon.png'
   ),
   (
     2,
     5,
     'Stock Trading',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti. ',
+    'Welcome to the ultimate online community forum for Java Programming enthusiasts! Join a vibrant community of developers and learners from all levels, where you can share your knowledge, ask questions, and engage in discussions about Java-related topics. Whether you\'re a seasoned programmer or just starting your coding journey, Guidance Exchange is the perfect place to connect, collaborate, and grow together in the world of Java Programming.',
     'stocks_circle.png'
   ),
   (
     3,
     4,
     'Coin Collecting',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi omnis, quod odio praesentium rem cupiditate soluta laborum aspernatur, explicabo temporibus veritatis a cumque quas dolor libero corrupti vero dolorum consequatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem ipsam libero iure fugit facere omnis, consequuntur dolor? In aperiam pariatur ex aut vel vitae reprehenderit sunt vero modi deleniti. ',
+    'Welcome to the ultimate online community forum for passionate numismatists! Immerse yourself in the fascinating world of coin collecting, where enthusiasts gather to share their expertise, showcase rare finds, and discuss the rich history behind these treasured pieces. Whether you\'re a seasoned collector or a newcomer with a growing interest, Guidance Exchange provides a welcoming environment to connect with like-minded individuals, expand your knowledge, and embark on a captivating journey through the realm of coins.',
     'coins_circle.png'
   );
